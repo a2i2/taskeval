8be08eacbcad664b12c71a52d6d7b72e3eefb9ef
ucokzeko <stefanus.kurniawan@deakin.edu.au>
2025-09-19 11:03:57 +1000

    ENHANCE: Add classification and clean up the UIs

--- ui/figure_extractions.py
+++ ui/figure_extractions.py
@@ -1,33 +1,5 @@
-import json
-
 import cairosvg
 import streamlit as st
-
-
-def figure_extractions_inputs():
-    """
-    Display the input fields for the figure extractions.
-    """
-    col1, col2 = st.columns([1, 1])
-    with col1:
-        # Text input for LLM instructions
-        instruction = st.text_area(
-            "Enter your instructions for the LLM to perform:",
-            placeholder="e.g., Extract the data points in the chart image and provide the output as a table...",
-            height=150,
-            help=(
-                "Provide clear instructions for what you want "
-                "the LLM to do with the uploaded image"
-            ),
-        )
-    with col2:
-        # File upload
-        uploaded_file = st.file_uploader(
-            "Choose an image file",
-            type=["png", "jpg", "jpeg", "gif", "bmp", "tiff"],
-            help="Upload the diagram for the LLM to process",
-        )
-    return instruction, uploaded_file
 
 
 def figure_extractions_results(image_path, results):
@@ -40,61 +12,7 @@ def figure_extractions_results(image_path, results):
         st.subheader("Original chart image")
         st.image(image_path)
     with col2:
-        st.subheader("Extracted table data")
-        st.dataframe(results["extracted_table_data"])
-
-    st.header("Evaluation Strategies")
-    # Add notes to say that only visualize strategy is supported for now
-    st.markdown(
-        (
-            "**Note:** Only 'visualize' strategy is supported for now. "
-            "Other strategies will be added in the future."
-        )
-    )
-    critical_errors = results["critical_errors"]
-    evaluation_strategies = results["error_strategy_mapping"]
-    rationales = results["mapping_rationale"]
-
-    if (
-        critical_errors
-        and len(
-            [eval for eval in evaluation_strategies.values() if eval == "visualize"]
-        )
-        > 0
-    ):
-        # Create tabs dynamically
-        tab_names = []
-        for error in critical_errors:
-            if evaluation_strategies[error["error_name"]] != "visualize":
-                continue
-            error_name = error["error_name"]
-            display_error_name = f"{error_name[0].upper()}{error_name[1:]}".replace(
-                "_", " "
-            )
-            tab_names.append(f"{display_error_name} ({error['likelihood']})")
-
-        tabs = st.tabs(tab_names)
-        tab_counter = 0
-        for error in critical_errors:
-            if evaluation_strategies[error["error_name"]] != "visualize":
-                continue
-            with tabs[tab_counter]:
-                error_name = error["error_name"]
-                evaluation_strategy = evaluation_strategies[error_name]
-                evaluation_strategy = (
-                    f"{evaluation_strategy[0].upper()}{evaluation_strategy[1:]}"
-                )
-                evaluation_strategy = evaluation_strategy.replace("_", " ")
-
-                st.markdown(f"**Description:** {error['description']}")
-                st.markdown(
-                    f"**Recommended evaluation strategy:** *{evaluation_strategy}*"
-                )
-                st.markdown(f"**Rationale:**\n{rationales[error_name]}")
-                tab_counter += 1
-
         st.subheader("Regenerated Chart Image")
-        # TODO: Add regenerated chart image
         if results["regenerated_chart_image"] == "":
             st.error("No regenerated chart image available")
         else:
@@ -102,17 +20,6 @@ def figure_extractions_results(image_path, results):
                 bytestring=results["regenerated_chart_image"].encode("utf-8")
             )
             st.image(png_bytes)
-    else:
-        st.info("No critical errors found.")
 
-    temp_results = results
-    temp_results["extracted_table_data"] = results["extracted_table_data"].to_dict(
-        orient="records"
-    )
-
-    # Make this collapsible
-    with st.expander("Raw Evaluation Results"):
-        st.code(
-            json.dumps(temp_results, indent=2, ensure_ascii=False),
-            language="json",
-        )
+    st.subheader("Extracted table data")
+    st.dataframe(results["extracted_table_data"])
--- ui/qa_answering.py
+++ ui/qa_answering.py
@@ -1,5 +1,3 @@
-import json
-
 import streamlit as st
 
 from ui.utils import (
@@ -8,37 +6,6 @@ from ui.utils import (
     highlight_text_multicolor,
     highlight_section_text,
 )
-
-
-def qa_answering_inputs():
-    """
-    Display the input fields for the QA answering.
-    """
-    col1, col2 = st.columns([1, 1])
-    with col1:
-        # Text input for LLM instructions
-        qa_question = st.text_area(
-            "Enter your question for the LLM to answer:",
-            placeholder="e.g., How do they perform multilingual training?",
-            height=150,
-            help="Provide clear question from the uploaded pdf",
-        )
-
-    with col2:
-        # File upload
-        uploaded_pdf = st.file_uploader(
-            "Choose an pdf file",
-            type=["pdf"],
-            help="Upload the pdf for the LLM to process",
-        )
-    expected_answer = st.text_area(
-        "Enter your expected answer for evaluating the LLM's answer:",
-        placeholder="e.g., Multilingual training is performed by randomly alternating between languages for every new minibatch",
-        height=100,
-        help="Provide clear expected answer from the uploaded pdf",
-    )
-    return qa_question, expected_answer, uploaded_pdf
-
 
 eval_label_mapping = {
     "AnswerRelevancyMetric": "Answer Relevancy",
@@ -68,99 +35,34 @@ def qa_answering_results(results):
     section_matches = get_section_specific_matches(
         qa_results["answer"], shortened_sources
     )
-    st.subheader("QA Answering Results")
-    st.write(qa_results["answer"])
+    st.header("QA Answering Results")
 
-    critical_errors = results["critical_errors"]
-    evaluation_strategies = results["error_strategy_mapping"]
-    rationales = results["mapping_rationale"]
+    st.subheader("Evaluation Metrics")
+    metrics = results["qa_scores"]
+    cols = st.columns(len(metrics))
+    for i, metric in enumerate(metrics.items()):
+        with cols[i]:
+            st.markdown(f"#### {eval_label_mapping[metric[0]]}: {metric[1]:.2f}")
 
-    if (
-        critical_errors
-        and len(
-            [
-                eval
-                for eval in evaluation_strategies.values()
-                if eval == "visualize" or eval == "automate"
-            ]
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Highlighted sections from the sources")
+        highlighted_output = highlight_text_multicolor(
+            qa_results["answer"], section_matches
         )
-        > 0
-    ):
-        # Create tabs dynamically
-        tab_names = []
-        for error in critical_errors:
-            if (
-                evaluation_strategies[error["error_name"]] != "visualize"
-                and evaluation_strategies[error["error_name"]] != "automate"
-            ):
-                continue
-            error_name = error["error_name"]
-            display_error_name = f"{error_name[0].upper()}{error_name[1:]}".replace(
-                "_", " "
-            )
-            tab_names.append(f"{display_error_name} ({error['likelihood']})")
-
-        tabs = st.tabs(tab_names)
-        tab_counter = 0
-        for error in critical_errors:
-            if (
-                evaluation_strategies[error["error_name"]] != "visualize"
-                and evaluation_strategies[error["error_name"]] != "automate"
-            ):
-                continue
-            with tabs[tab_counter]:
-                error_name = error["error_name"]
-                evaluation_strategy = evaluation_strategies[error_name]
-                evaluation_strategy = (
-                    f"{evaluation_strategy[0].upper()}{evaluation_strategy[1:]}"
-                )
-                evaluation_strategy = evaluation_strategy.replace("_", " ")
-
-                st.markdown(f"**Description:** {error['description']}")
-                st.markdown(
-                    f"**Recommended evaluation strategy:** *{evaluation_strategy}*"
-                )
-                st.markdown(f"**Rationale:**\n{rationales[error_name]}")
-                tab_counter += 1
-
-                if evaluation_strategies[error_name].lower() == "visualize":
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        st.subheader("Highlighted sections from the sources")
-                        highlighted_output = highlight_text_multicolor(
-                            qa_results["answer"], section_matches
-                        )
-                        st.markdown(
-                            highlighted_output,
-                            unsafe_allow_html=True,
-                        )
-                    with col2:
-                        st.subheader("Sources")
-                        for i, node in enumerate(shortened_sources):
-                            color = HIGHLIGHT_COLORS[i % len(HIGHLIGHT_COLORS)]
-                            # Highlight keywords in this section if it has matches
-                            if i in section_matches:
-                                highlighted_section = highlight_section_text(
-                                    node["text"].strip(), section_matches[i], color
-                                )
-                            else:
-                                highlighted_section = node["text"].strip()
-                            st.markdown(highlighted_section, unsafe_allow_html=True)
-                elif evaluation_strategies[error_name].lower() == "automate":
-                    st.subheader("Evaluation Metrics")
-                    metrics = results["qa_scores"]
-                    cols = st.columns(len(metrics))
-                    for i, metric in enumerate(metrics.items()):
-                        with cols[i]:
-                            st.markdown(
-                                f"#### {eval_label_mapping[metric[0]]}: {metric[1]}"
-                            )
-    else:
-        st.info("No critical errors found.")
-
-    # Make this collapsible
-    with st.expander("Raw Results"):
-        st.code(
-            json.dumps(results, indent=2, ensure_ascii=False),
-            language="json",
+        st.markdown(
+            highlighted_output,
+            unsafe_allow_html=True,
         )
+    with col2:
+        st.subheader("Sources")
+        for i, node in enumerate(shortened_sources):
+            color = HIGHLIGHT_COLORS[i % len(HIGHLIGHT_COLORS)]
+            # Highlight keywords in this section if it has matches
+            if i in section_matches:
+                highlighted_section = highlight_section_text(
+                    node["text"].strip(), section_matches[i], color
+                )
+            else:
+                highlighted_section = node["text"].strip()
+            st.markdown(highlighted_section, unsafe_allow_html=True)
